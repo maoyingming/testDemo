8b856dbe9a1db1cd82e7e66025d232533fb69b21
Rick <mym_0225@163.com>
2019-12-02 16:10:33 +0800

    test

---
+++ MyGreeter/Client.php
@@ -0,0 +1,18 @@
+<?php
+namespace MyGreeter;
+class Client{
+	function __construct(){
+	}
+	public function getGreeting(){
+		date_default_timezone_set('Asia/Shanghai');
+		$h = date("H");
+		if($h < 12){
+			echo "早上好!";
+		}else if($h < 18){
+			echo "下午好！";
+		}else{
+			echo "晚上好！";
+		}
+	}
+}
+?>
